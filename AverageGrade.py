b5bb968e330887add33c55dfd310a412816acae7
nekit1338 <47562425+nekit1338@users.noreply.github.com>
2024-07-06 19:42:53 +0500

    Update AverageGrade.py

--- AverageGrade.py
+++ AverageGrade.py
@@ -3,14 +3,17 @@ students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
 
 
 students_list = list(students)  # Set -> List
-students_list.sort(key=lambda x: x[0])  # Сортировка списка студентов в алфавитном порядке
+students_list.sort(key=lambda x: x[0])  # Сортировка списка студентов по первому символу (в алфавитном порядке)
+
+
 average_grades = []  # Новый список для средних баллов
 for i in grades:
     average = sum(i)/len(i)
     average_grades.append(average)  # Заполнение списка результатами расчета average
+
+
 average_grades_round = []  # Новый список с 2 знакам после запятой
 for j in average_grades:
     average_grades_round.append(round(j, 2))
-print(average_grades_round)
 students_dict = dict(zip(students_list, average_grades_round))
 print(students_dict)
