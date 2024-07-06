ffb6617294cb69a748799edcc950e72d2627c703
nekit1338 <47562425+nekit1338@users.noreply.github.com>
2024-07-06 19:49:36 +0500

    Update AverageGrade.py

--- AverageGrade.py
+++ AverageGrade.py
@@ -7,8 +7,8 @@ students_list.sort(key=lambda x: x[0])  # Сортировка списка ст
 
 
 average_grades = []  # Новый список для средних баллов
-for i in grades:
-    average = sum(i)/len(i)
+for i in grades:  # Проходимся циклом "for in" по списку grades 
+    average = sum(i)/len(i)  # sum(i) - сумма элементов вложенных списков внутри grades; len(i) - длина i-го списка
     average_grades.append(average)  # Заполнение списка результатами расчета average
 
 
